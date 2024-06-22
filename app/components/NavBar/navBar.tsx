63fb299f661a7b4662e47822866678bd44971c13
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2024-06-23 00:32:18 +0100

    name changeds according to folder

--- app/components/NavBar/navBar.tsx
+++ app/components/NavBar/navBar.tsx
@@ -1,5 +1,6 @@
 import cx from "clsx";
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import {
   Container,
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
-import NavBar from "./components/NavBar/page";
-
+import NavBar from "./components/NavBar/navBar";
 
 import type { AppProps } from "next/app";
 import { createTheme, MantineProvider } from "@mantine/core";
