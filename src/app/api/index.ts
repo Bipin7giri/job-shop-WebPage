c5f01657091bca17953ba922234d8ba398807c95
Dirtybug36 <104801789+Dirtybug36@users.noreply.github.com>
2024-09-06 16:46:58 +0100

    api changed

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://j-deploy.onrender.com/api",
+  // baseURL: "https://j-deploy.onrender.com/api",
+  baseURL: "https://job-shop.bipingiri77.com.np/api/",
   headers: {
     Accept: "*/*",
     "Content-Type": "application/json",
